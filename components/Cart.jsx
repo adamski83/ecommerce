324dd956cc9b2bbe28f76aab99ac77eb9a809a11
adamski83 <adamsik83@wp.pl>
2022-11-13 17:24:28 +0100

    remove unused onClick in Cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -76,9 +76,7 @@ const Cart = () => {
 													}>
 													<AiOutlineMinus />
 												</span>
-												<span
-													className="num"
-													onClick="">
+												<span className="num">
 													{item.quantity}
 												</span>
 												<span
